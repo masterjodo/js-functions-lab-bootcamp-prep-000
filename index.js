b3a90161483ac7bb06f6d40f3f6e0dd05185cb3d
Jawad Ashour <jawadashour@gmail.com>
2018-12-21 18:40:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ function happyHolidayTo(holiday, name) {
 }
 
 function holidayCountdown(days, holiday) {
-  return ('It\'s ${days} days to ${holiday}!')
+  return ('It\'s ${days} days until ${holiday}!')
 }
